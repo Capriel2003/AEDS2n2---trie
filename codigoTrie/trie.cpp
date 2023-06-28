c0e826766499e703bb0309083d3a6162fe175a30
Capriel2003 <gabrielbraganca20039@gmail.com>
2023-06-28 20:53:55 -0300

    funfando

--- codigoTrie/trie.cpp
+++ codigoTrie/trie.cpp
@@ -4,9 +4,23 @@
 #include <string>
 #include <regex>
 #include <algorithm>
+#include <fstream>
 using namespace std;
 
 const int TamAlfabeto = 26;
+
+
+vector<string> separaFrase(string frase){
+    vector<string> separado;
+
+    stringstream ss(frase);
+    string pal;
+    while (ss >> pal) {
+        separado.push_back(pal);
+    }
+
+    return separado;
+}
 
 string tratarTexto(const string& texto) {
     // Converter caracteres maiúsculos para minúsculos
@@ -23,30 +37,38 @@ string tratarTexto(const string& texto) {
     return textoTratado;
 }
 
-struct Ocorrencia {
-    int posicao;
-    Ocorrencia* proximo;
-    Ocorrencia(int pos) : posicao(pos), proximo(nullptr) {
-        //vazio
-    }
+class Ocorrencia {
+    public:
+        int posicao;
+        Ocorrencia* proximo;
+        Ocorrencia(int pos) : posicao(pos), proximo(nullptr) {
+            //vazio
+        }
+        friend std::ostream& operator<<(std::ostream& os, const Ocorrencia& ocorrencia) {
+            os << "Posicao: " << ocorrencia.posicao;
+            return os;
+        }
 };
 
-struct Documento {
-    string nome;
-    list<Ocorrencia*> ocorrencias;
-    Documento* proximo;
-    Documento(const string& docNome) : nome(docNome), proximo(nullptr) {
-        //construtor vazio com nenhuma entrada
-    }
+
+class Documento {
+    public:
+        string nome;
+        list<Ocorrencia*> ocorrencias;
+        Documento* proximo;
+        Documento(const string& docNome) : nome(docNome), proximo(nullptr) {
+            //construtor vazio com nenhuma entrada
+        }
 };
 
-struct NoTrie {
-    string chave;
-    list<Documento*> documentos;
-    vector<NoTrie*> filhos;
-    NoTrie(const string& chaveNo) : chave(chaveNo), filhos(TamAlfabeto, nullptr) {
-        //contrutor NoTrie vazio
-    }
+class NoTrie {
+    public:
+        string chave;
+        list<Documento*> documentos;
+        vector<NoTrie*> filhos;
+        NoTrie(const string& chaveNo) : chave(chaveNo), filhos(TamAlfabeto, nullptr) {
+            //contrutor NoTrie vazio
+        }
 };
 
 class Trie {
@@ -89,24 +111,49 @@ public:
         documentoNo->ocorrencias.push_back(ocorrenciaNo);
     }
 
-    list<Ocorrencia*> buscar(const string& palavra) {
-        list<Ocorrencia*> ocorrencias;
+    map<string, list<Ocorrencia*>> buscaPalavra(const string& palavra) {
+        map<string, list<Ocorrencia*>> ocorrencias;
 
         NoTrie* atual = raiz;
 
         for (char c : palavra) {
             int indice = c - 'a';
             if (atual->filhos[indice] == nullptr) {
-                // Palavra não encontrada, retorna lista vazia
+                // Palavra não encontrada, retorna vazio
                 return ocorrencias;
             }
             atual = atual->filhos[indice];
         }
 
-        // Encontrou a palavra, adiciona as ocorrências de cada documento à lista
+        // Encontrou a palavra, comeca a incrementar essas ocorrencias numa lista
         for (Documento* doc : atual->documentos) {
             for (Ocorrencia* ocorrencia : doc->ocorrencias) {
-                ocorrencias.push_back(ocorrencia);
+                ocorrencias[doc->nome].push_back(ocorrencia);
+            }
+        }
+
+        return ocorrencias;
+    }
+
+    map<string,list<list<Ocorrencia*>>> buscaFrase(const string& fraseInt){
+        vector <string> frase = separaFrase(fraseInt);
+        map<string, list<list<Ocorrencia*>>> ocorrencias;
+        map<string, list<Ocorrencia*>> aux;
+
+        for(auto x: frase){
+            aux = buscaPalavra(x);
+            for(auto y: aux){
+                ocorrencias[y.first].push_back(y.second);
+            }
+        }
+
+        for (const auto& ocorrencia : ocorrencias) {
+            cout << "Documento: " << ocorrencia.first << endl;
+            for (const auto& listaOcorrencias : ocorrencia.second) {
+                for (const auto& ocorrenciaPtr : listaOcorrencias) {
+                    cout << "Palavra: " << x << endl;
+                    cout << "Posicao: " << ocorrenciaPtr->posicao << endl;
+                }
             }
         }
 
@@ -117,24 +164,53 @@ public:
 int main() {
     Trie trie;
     trie.inserir("exemplo", "documento1.txt", 10);
-    trie.inserir("exemplo", "documento1.txt", 4);
-    trie.inserir("examplo", "documento5.txt", 50);
-    trie.inserir("exam", "documento2.txt", 5);
+    trie.inserir("exemplo", "documento2.txt", 4);
+    trie.inserir("seu", "documento5.txt", 50);
+    trie.inserir("pai", "documento2.txt", 5);
     trie.inserir("exem", "documento2.txt", 5);
 
 
     string palavraBusca = "exemplo";
-    list<Ocorrencia*> ocorrencias = trie.buscar(palavraBusca);
+    string fraseBusca = "seu pai eh corno";
+    map<string, list<Ocorrencia*>> ocorrenciasPalavra = trie.buscaPalavra(palavraBusca);
+    map<string, list<list<Ocorrencia*>>> ocorrenciaFrase = trie.buscaFrase(fraseBusca);
+    
 
-    if (ocorrencias.empty()) {
+
+
+
+
+    if (ocorrenciasPalavra.empty()) {
         cout << "Palavra '" << palavraBusca << "' nao encontrada." << endl;
     } 
     else {
         cout << "Ocorrencias da palavra '" << palavraBusca << "':" << endl;
-        for (Ocorrencia* ocorrencia : ocorrencias) {
-            cout << "Posicao: " << ocorrencia->posicao << endl;
+        for (auto ocorrencia : ocorrenciasPalavra) {
+            cout << ocorrencia.first << endl;
+            for(auto x: ocorrencia.second){
+                cout << *x <<endl;
+            }
         }
     }
+
+
+
+    if (ocorrenciaFrase.empty()) {
+        cout << "Palavra '" << fraseBusca << "' nao encontrada." << endl;
+    } 
+    else {
+        cout << "Ocorrencias da palavra '" << fraseBusca << "':" << endl;
+        for (auto ocorrencia : ocorrenciaFrase) {
+            cout << ocorrencia.first << endl;
+            for(auto x: ocorrencia.second){
+                for(auto y: x){
+                    cout << *y <<endl;
+                }
+            }
+        }
+    }
+
+
 
     return 0;
 }
